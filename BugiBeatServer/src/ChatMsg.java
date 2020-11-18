14d8147ec389a7d4fe6611a7aa7be64617a300b5
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-11-18 13:40:00 +0900

    📲 feat : enable socket communication

---
+++ BugiBeatServer/src/ChatMsg.java
@@ -0,0 +1,20 @@
+
+
+//ChatMsg.java 채팅 메시지 ObjectStream 용.
+import java.io.Serializable;
+
+import javax.swing.ImageIcon;
+
+class ChatMsg implements Serializable {
+	private static final long serialVersionUID = 1L;
+	public String code; // 100:로그�?�, 400:로그아웃, 200:채팅메시지, 300:Image
+	public String UserName;
+	public String data;
+	public ImageIcon img;
+
+	public ChatMsg(String UserName, String code, String msg) {
+		this.code = code;
+		this.UserName = UserName;
+		this.data = msg;
+	}
+}
